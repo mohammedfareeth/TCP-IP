dc9596028fb1124035a72628b94525c82230b0b4
Mohammed Fareeth <mohammedfareethfero@gmail.com>
2025-12-02 08:39:32 +0530

    Add TCP concurrent server implementation using Fork()
    Implement a TCP concurrent server using fork() to handle multiple client connections.

---
+++ SOCKET PROGRAMMING/SER & CLI PROGRAM/Concurrent Server.c
@@ -0,0 +1,67 @@
+/*TCP concurrent server using fork()*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<strings.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+int main(int argc,char *argv[])
+{
+	int sockfd,newsfd;
+	struct sockaddr_in srv,cln;
+	char rdbuf[128];
+
+        /**********SOCKET FUNCTION**********/
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
+	if(sfd<0)
+	{
+		perror("socket");
+		return 0;
+	}
+	printf("socket created successfully!!....\n");
+	srv.sin_family         =AF_INET;
+	srv.sin_port           =htons(atoi(argv[1]));
+	srv.sin_addr.s_addr    =inet_addr("0.0.0.0");
+	
+        /**********BIND FUNCTION**********/
+	if(bind(sockfd,(struct sockaddr *)&srv,sizeof(srv))==0)
+		printf("bind successfull!!\n");
+	else
+	{
+		perror("bind");
+		return 0;
+	}
+
+        /**********LISTEN FUNCTION**********/
+	listen(sockfd,1);
+	int len=sizeof(cln);
+	while(1)
+	{
+	printf("waiting for connection!!\n");
+
+        /**********ACCEPT FUNCTION**********/
+	newsfd=accept(sockfd,(struct sockaddr *)&cln,&len);
+	if(newsfd<0)
+	{
+		perror("accept");
+		return 0;
+	}
+	printf("connection accepted!!\n");
+
+        /**********READ FUNCTION**********/
+	if(fork()==0)
+	{	
+	while(1)
+	{	
+	bzero(rdbuf,128);
+	if(read(newsfd,rdbuf,128)==0)
+	{
+		printf("client%d terminated!!\n",newsfd-sockfd);
+		exit(0);
+	}
+	printf("received string is :%s\n",rdbuf);
+	}
+	}
+	}
+}
